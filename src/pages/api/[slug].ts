732647657c44aa865507e3484eb93f07d7e51699
Morgan <morgan.willock@gmail.com>
2022-08-17 13:22:29 +0930

    Added redirect to url if shortUrl is found and 404 if no record is found

--- src/pages/api/[slug].ts
+++ src/pages/api/[slug].ts
@@ -6,17 +6,22 @@ const getUrl = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
 
   if (typeof slug !== 'string') {
-    res.statusCode = 404
+    res.statusCode = 404;
     return res.send({ message: 'please use a slug' })
   }
 
-  const url = await prisma.shortUrl.findFirst({
+  const shortUrl = await prisma.shortUrl.findFirst({
     where: {
       slug
     },
   });
 
-  res.status(200).json(url);
+  if (shortUrl) {
+    res.redirect(shortUrl.url)
+  }
+
+  res.statusCode = 404;
+  return res.send({ message: 'url not found!' })
 };
 
 export default getUrl;
