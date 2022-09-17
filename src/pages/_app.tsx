3c2ad4a6bec530bd271e3b1af6f920f4f35a435a
Morgan <morgan.willock@gmail.com>
2022-09-17 15:18:40 +0930

    Adjusted tRPC config url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,9 +21,7 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-     ? `https://${process.env.VERCEL_URL}/api/trpc`
-     : 'http://localhost:3000/api/trpc';
+    const url = getBaseUrl();
 
     return {
       url,
