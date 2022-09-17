ea73627c24f2285580f39a0d7a5c94131de89f83
Morgan <morgan.willock@gmail.com>
2022-09-17 15:11:21 +0930

    Added vercel config for tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,7 +21,9 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = `${getBaseUrl()}/api/trpc`;
+    const url = process.env.VERCEL_URL
+     ? `https://${process.env.VERCEL_URL}/api/trpc`
+     : 'http://localhost:3000/api/trpc';
 
     return {
       url,
@@ -35,5 +37,5 @@ export default withTRPC<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: false,
+  ssr: true,
 })(MyApp);
